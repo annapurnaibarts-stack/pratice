4cc38f19493e9b100883416c373d646c40b7ef50
annapurnaibarts-stack <annapurna.ibarts@gmail.com>
2026-05-06 12:55:04 +0000

    TodoContext Changes

--- todo/src/contexts/index.js
+++ todo/src/contexts/index.js
@@ -1,6 +1,6 @@
-// export {TodoContext,TodoProvider,useTodo} from './TodoContext'
+export {TodoContext,TodoProvider,useTodo} from './TodoContext'
 import TodoItem from "../components/TodoItem";
 import TodoForm from "../components/TodoForm";
 
-
+export {TodoForm, TodoItem}
 
---
+++ todo2/src/contexts/index.js
@@ -0,0 +1,6 @@
+export { TodoContext , TodoProvider } from "./TodoContext";
+
+import TodoForm from "../components/TodoForm";
+import TodoItem from "../components/TodoItem";
+
+export {TodoForm,TodoItem}
